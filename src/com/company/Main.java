d2c9cf571b9dc0c99de3268a00ded5397c5cd969
User <khayrullina.k.i@edu.mirea.ru>
2022-02-09 00:10:00 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите объем сейфа: ");
+        int value = Integer.parseInt(in.next());
+        System.out.print("Введите количество предметов: ");
+        int b = Integer.parseInt(in.next());
+        Items[] items = new Items[b];
+        for (int i = 0; i < b; i++) {
+            System.out.print("Введите объем предмета: ");
+            int tempValue = Integer.parseInt(in.next());
+            System.out.print("Введите цену предмета: ");
+            int tempPrice = Integer.parseInt(in.next());
+            System.out.print("Введите имя предмета: ");
+            String tempName = in.next();
+            items[i] = new Items(tempValue, tempPrice, tempName);
+        }
+        Safe safe = new Safe(value);
+        safe.enumeration(items);
+        safe.solution();
+    }
+}
